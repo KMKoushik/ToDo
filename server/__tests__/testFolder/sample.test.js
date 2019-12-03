0523ca84dfceadfe6efa16ae7f7ac4306ef407d7
Koushik Mohan <koushikmohan1996@gmail.com>
2019-12-03 12:10:24 +0530

    test 2

--- server/__tests__/testFolder/sample.test.js
+++ server/__tests__/testFolder/sample.test.js
@@ -2,4 +2,8 @@ describe('Sample test suit', () => {
   test('Sample test case', () => {
     expect(true).toBe(true);
   })
+
+  test('Sample test case 2', () => {
+    expect(true).toBe(true);
+  })
 });
